5d1bfab179af2e16853ca5c633327a4c2991706b
ykcoda <yawkyereh@outlook.com>
2025-03-06 11:05:33 +0000

    aws resourced added to master branch

--- azurerm.tf
+++ azurerm.tf
@@ -16,18 +16,3 @@ resource "aws_instance" "app-server" {
   }
   
 }
-
-
-provider "aws" {
-  region = "us-east-1"
-}
-
-
-resource "aws_instance" "app-server" {
-  ami = "ami-05b10e08d247fb927"
-  instance_type = "t2.micro"
-  tags = {
-    Name = "app-server"
-  }
-  
-}
