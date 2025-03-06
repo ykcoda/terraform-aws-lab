c10a382ee3a3f9b614706c6a9b87fafd5995b6d0
ykcoda <yawkyereh@outlook.com>
2025-03-06 11:08:23 +0000

    aws db server resource added

--- azurerm.tf
+++ azurerm.tf
@@ -14,5 +14,13 @@ resource "aws_instance" "app-server-1" {
   tags = {
     Name = "app-server-1"
   }
+}
+
+resource "aws_instance" "db-server" {
+  ami = "ami-05b10e08d247fb927"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "db-server"
+  }
   
 }
