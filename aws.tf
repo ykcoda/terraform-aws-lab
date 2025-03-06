79aef5d4af82fdc33695210a824de3764641f110
ykcoda <yawkyereh@outlook.com>
2025-03-06 10:47:18 +0000

    add 1st AWS resource and .gitignore file

---
+++ aws.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "web-server" {
+  ami = "ami-05b10e08d247fb927"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "web-server"
+  }
+  
+}
