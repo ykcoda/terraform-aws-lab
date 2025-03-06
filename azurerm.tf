162db9b865ef91de34440f7a0eabb9868a18d52e
ykcoda <yawkyereh@outlook.com>
2025-03-06 11:00:39 +0000

    added an AWS resource (app-server) to azurerm.tf

--- azurerm.tf
+++ azurerm.tf
@@ -1,3 +1,18 @@
 provider "azurerm" {
   
 }
+
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+resource "aws_instance" "app-server" {
+  ami = "ami-05b10e08d247fb927"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "app-server"
+  }
+  
+}
