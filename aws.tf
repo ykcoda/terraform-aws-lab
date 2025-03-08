f59a0fbea99f4967f0538958fb9eacab17ed0dff
ykcoda <yawkyereh@outlook.com>
2025-03-08 07:41:05 +0000

    created a yml for replicationController

--- aws.tf
+++ aws.tf
@@ -6,8 +6,23 @@ resource "aws_instance" "game-server" {
   ami = "ami-05b10e08d247fb927"
   instance_type = "t2.micro"
   tags = {
-    Name = "game-server"
+    Name = "$(local.environment)-server"
   }
 }
 
+locals {
+    environment = "test"
+}
 
+variable "devops-ami" {
+  default = "ami-05b10e08d247fb927"
+  type = string
+}
+
+output "output-ami" {
+  value = var.devops-ami 
+}
+
+output "show-environment" {
+  value = local.environment
+}
